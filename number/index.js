6591ea15ad2eae358ddbc92b46f4ac9e5cc62493
kwokth4 <118238880+kwokth4@users.noreply.github.com>
2022-11-18 17:23:56 +0800

    Create index.js

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for(var i = 1; i <= 20; i++){
+    process.stdout.write(i+" is ");
+    if(i%2 == 0){
+      console.log("even number");
+    }
+    else {
+      console.log("odd number");
+    }
+}
