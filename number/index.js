edd4aace368ede4f90faa645192c5ac4c187e3c0
kwokth4 <118238880+kwokth4@users.noreply.github.com>
2022-11-18 17:24:16 +0800

    Update index.js

--- number/index.js
+++ number/index.js
@@ -1,9 +1,9 @@
 for(var i = 1; i <= 20; i++){
     process.stdout.write(i+" is ");
     if(i%2 == 0){
-      console.log("even number");
+      console.log("even number.");
     }
     else {
-      console.log("odd number");
+      console.log("odd number.");
     }
 }
